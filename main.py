4282c6725938b7a4530b5e522edba245dcce0404
JohannesSchnell <j.schnell@52north.org>
2023-05-30 17:30:09 +0200

    added xml files

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-from sld import Heatmap
-import os
 
-print(os.getcwd())
-
-wave_height = Heatmap(layerName="waveh", styleName="waveh", catNums=[-1, 2, 4, 7],
-                      catColors=['#1ce3ed',
-                                 '#148818',
-                                 '#ba5802',
-                                 '#ba0e02'])
-wave_height.printSld()
-wave_height.writeSld("345")
